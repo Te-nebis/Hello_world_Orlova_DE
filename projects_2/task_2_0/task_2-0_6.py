7bd96beaed6875bc816e2fbd776e496fe282284b
Orlova Diana <whyhen@yandex.ru>
2026-02-22 04:10:30 +0300

    Add task_2_0 files

---
+++ projects_2/task_2_0/task_2-0_6.py
@@ -0,0 +1,7 @@
+with open("C:/Users/gusen/OneDrive/Рабочий стол/Инфа/output.txt", "w", encoding="utf-8") as file:
+    file.write("Имя: Диана\n")
+    file.write("Фамилия: Орлова\n")
+    file.write("Курс: 1\n")
+    file.write("Группа: 4731204/50002\n")
+    file.write("Институт: Институт биомедицинских систем и биотехнологий\n")
+    file.write("Факультет: Биотехнические системы и технологии")
