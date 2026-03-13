40b39e0a53a59dc173068ab0419e7b6fd698fbae
Orlova Diana <whyhen@yandex.ru>
2026-03-13 11:19:54 +0300

    Update files_roundtrip.sh

--- projects_3/task_3_10/files_roundtrip.sh
+++ projects_3/task_3_10/files_roundtrip.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-
-# Создаем 10 текстовых файлов
+#Создание 
 for i in {1..10}
 do
     touch "test$i.txt"
 done
 
 echo "Файлы созданы: test1.txt, test2.txt, ..., test10.txt"
-
-# Удаляем файлы в обратном порядке
+№Удаление(тот же порядок)
 i=10
 while [ $i -ge 1 ]
 do
