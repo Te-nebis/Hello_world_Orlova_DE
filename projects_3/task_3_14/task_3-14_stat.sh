8bcd0fd950f23f64d7df2ed50fcc01f933da35be
Orlova Diana <whyhen@yandex.ru>
2026-03-13 11:23:16 +0300

    Update task_3-14_stat.sh

--- projects_3/task_3_14/task_3-14_stat.sh
+++ projects_3/task_3_14/task_3-14_stat.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-# Проверяем, существует ли файл students.txt
 if [ ! -f students.txt ]; then
     echo "Файл students.txt не найден!"
     exit 1
@@ -8,14 +6,11 @@ fi
 
 echo "=== Статистика по оценкам студентов ==="
 
-# Сумма всех оценок
 sum=$(awk '{sum += $2} END {print sum}' students.txt)
 echo "Сумма всех оценок: $sum"
 
-# Средняя оценка
 average=$(awk '{sum += $2} END {printf "%.2f", sum/NR}' students.txt)
 echo "Средняя оценка: $average"
 
-# Максимальная оценка
 max=$(awk 'NR==1{max=$2} $2>max{max=$2} END {print max}' students.txt)
 echo "Максимальная оценка: $max"
