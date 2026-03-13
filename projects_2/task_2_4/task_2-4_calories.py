b7dbbd5775f2b1c0ba0dde16e753011106008a16
Orlova Diana <whyhen@yandex.ru>
2026-03-13 05:31:45 +0300

    Update task_2-4_calories.py

--- projects_2/task_2_4/task_2-4_calories.py
+++ projects_2/task_2_4/task_2-4_calories.py
@@ -2,5 +2,5 @@ protein_mass = int(input("Введите массу белка в продукт
 fats_mass = int(input("Введите массу жиров в продукте:"))
 carbohydrates_mass = int(input("Введите массу углеводов в продукте:"))
 cal = protein_mass * 4 + fats_mass * 9 + carbohydrates_mass * 4
-
 print(f"Общая калорийность продукта составляет {cal} ккал.")
+
