fd5537f769c886846036143d6a5738daee21d258
Orlova Diana <whyhen@yandex.ru>
2026-03-13 05:40:02 +0300

    Update task_2-7_3.py

--- projects_2/task_2_7/task_2-7_3.py
+++ projects_2/task_2_7/task_2-7_3.py
@@ -1,10 +1,8 @@
 seq = [ "ATATACGCGTA", "CTTCGGNGGA" ]
-
 for name in seq:
     print(f"\nПоследовательность: {name}")
     print(f"Построчно:")
-
     for latter in name:
         print(latter)
-
 print("Цикл выполнен")
+
