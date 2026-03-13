122ae1b6de96d9cce26b1095a5786791c4a1a2a8
Orlova Diana <whyhen@yandex.ru>
2026-03-13 05:30:34 +0300

    Update task_2-4_bmi.py

--- projects_2/task_2_4/task_2-4_bmi.py
+++ projects_2/task_2_4/task_2-4_bmi.py
@@ -1,5 +1,6 @@
 weight = float(input("Введите ваш вес (кг):"))
 height = float(input("Введите ваш рост (см):"))
-imt = weight / (height ** 2)
+imt = weight / ((height / 100) ** 2)
 print("--- Отчет о состоянии здоровья ---")
+
 print(f"Рост:\t{height} см\nВес:\t{weight} кг\nИндекс массы тела: {imt:.2f}")
